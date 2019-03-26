6df900beefa388ee984d4da232dacbf2927e0137
Rick Yiu <rickyiu@google.com>
2019-03-26 15:26:25 +0800

    sched/fair: refine some scheduler changes from AU drop
    Refine some changes from AU90. One is to allow boosted task run on min
    capacity cpu if it fits in. The other is to check fast exit for
    prefer-idle task first.
    
    Bug: 128477368
    Bug: 130576120
    Test: task rq selection behavior is as expected
    
    Change-Id: Ied57b37a361ed137d10167f0346f52a149d08cd6
    Signed-off-by: Rick Yiu <rickyiu@google.com>
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: Kailash <kailash.sudhakar@gmail.com>
    Signed-off-by: MadeOfGreat <ravenklawasd@gmail.com>
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7573,6 +7573,9 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 
 	} while (sg = sg->next, sg != sd->groups);
 
+	if (prefer_idle && (best_idle_cpu != -1))
+		return best_idle_cpu;
+
 	if (best_idle_cpu != -1 && !is_packing_eligible(p, target_cpu, fbt_env,
 					active_cpus_count, best_idle_cstate,
 					boosted)) {
@@ -7601,9 +7604,6 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 	 *   a) ACTIVE CPU: target_cpu
 	 *   b) IDLE CPU: best_idle_cpu
 	 */
-
-	if (prefer_idle && (best_idle_cpu != -1))
-		return best_idle_cpu;
 
 	if (target_cpu == -1)
 		target_cpu = prefer_idle
