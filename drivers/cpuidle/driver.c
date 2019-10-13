8ba6675bce2823b1878c185d16319ed2fac2a3cc
darkhz <kmachanwenw@gmail.com>
2019-10-13 10:04:41 +0530

    cpuidle: Multiple changes
    - Changes made from commit https://github.com/torvalds/linux/commit/259231a045616c4101d023a8f4dcc8379af265a6
    - Changes made to drivers/cpuidle/ from https://github.com/torvalds/linux/tree/259231a045616c4101d023a8f4dcc8379af265a6/drivers/cpuidle
    - Fixed compiler errors

--- drivers/cpuidle/driver.c
+++ drivers/cpuidle/driver.c
@@ -18,6 +18,8 @@
 
 #include "cpuidle.h"
 
+#define POLL_IDLE_RELAX_COUNT	200
+
 DEFINE_SPINLOCK(cpuidle_driver_lock);
 
 #ifdef CONFIG_CPU_IDLE_MULTIPLE_DRIVERS
@@ -181,10 +183,28 @@ static void __cpuidle_driver_init(struct cpuidle_driver *drv)
 static int __cpuidle poll_idle(struct cpuidle_device *dev,
 			       struct cpuidle_driver *drv, int index)
 {
+	u64 time_start = local_clock();
+
+	dev->poll_time_limit = false;
+
 	local_irq_enable();
 	if (!current_set_polling_and_test()) {
-		while (!need_resched())
+		unsigned int loop_count = 0;
+		u64 limit;
+
+		limit = cpuidle_poll_time(drv, dev);
+
+		while (!need_resched()) {
 			cpu_relax();
+			if (loop_count++ < POLL_IDLE_RELAX_COUNT)
+				continue;
+
+			loop_count = 0;
+			if (local_clock() - time_start > limit) {
+				dev->poll_time_limit = true;
+				break;
+			}
+		}
 	}
 	current_clr_polling();
 
