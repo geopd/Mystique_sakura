bdf2b6a66a9a677782a157fc787075d460df4300
darkhz <kmachanwenw@gmail.com>
2020-06-03 13:00:43 +0530

    cpufreq: schedutil: Tune specifically for the device

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -872,15 +872,10 @@ static int sugov_init(struct cpufreq_policy *policy)
 		goto stop_kthread;
 	}
 
-	/*
-	 * NOTE:
-	 * intializing up_rate/down_rate to 0 explicitly in kernel
-	 * since WALT expects so by default.
-	 */
-	tunables->up_rate_limit_us = 0;
-	tunables->down_rate_limit_us = 0;
+	tunables->up_rate_limit_us = 500;
+	tunables->down_rate_limit_us = 2000;
 	tunables->hispeed_load = DEFAULT_HISPEED_LOAD;
-	tunables->hispeed_freq = 0;
+	tunables->hispeed_freq = 1401600;
 
 	policy->governor_data = sg_policy;
 	sg_policy->tunables = tunables;
