6c322c3ab87c6712fc91521ec43de3f6ad73d2e7
tytydraco <tylernij@gmail.com>
2020-07-13 06:50:14 +0530

    msm: sde: Use interruptible waits
    Allow these tasks to be preempted in order to reduce latency.
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- drivers/media/platform/msm/sde/rotator/sde_rotator_r3.c
+++ drivers/media/platform/msm/sde/rotator/sde_rotator_r3.c
@@ -1968,7 +1968,7 @@ static u32 sde_hw_rotator_wait_done_regdma(
 	if (rot->irq_num >= 0) {
 		SDEROT_DBG("Wait for REGDMA completion, ctx:%p, ts:%X\n",
 				ctx, ctx->timestamp);
-		rc = wait_event_timeout(ctx->regdma_waitq,
+		rc = wait_event_interruptible_timeout(ctx->regdma_waitq,
 				!sde_hw_rotator_pending_swts(rot, ctx, &swts),
 				ctx->sbuf_mode ?
 				msecs_to_jiffies(KOFF_TIMEOUT_SBUF) :
