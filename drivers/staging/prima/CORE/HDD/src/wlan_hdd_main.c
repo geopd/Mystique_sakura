eff71e6b007528291e6aa611231b72506e880575
Albert I <kras@raphielgang.org>
2020-12-03 00:46:26 +0800

    prima: Fix non-diag build
    * Guard WLAN_LOGGING_SOCK_SVC_ENABLE under BUILD_DIAG_VERSION=1
    * This is potentially disastrous for getting logs from WLAN driver which
      highly depends on diag features, so handle with care.
    
    Test: Compile with BUILD_DIAG_VERSION={0|1}, build completes
    [Forward-port to LA.UM.9.6.2.r1]
    Signed-off-by: Albert I <kras@raphielgang.org>
    (cherry picked from commit 8b38c7581a4726c88f825d49af8d6e96feaf9ddd)

--- drivers/staging/prima/CORE/HDD/src/wlan_hdd_main.c
+++ drivers/staging/prima/CORE/HDD/src/wlan_hdd_main.c
@@ -12851,9 +12851,9 @@ void hdd_wlan_exit(hdd_context_t *pHddCtx)
    send_btc_nlink_msg(WLAN_MODULE_DOWN_IND, 0);
 
    hdd_close_tx_queues(pHddCtx);
+#ifdef WLAN_LOGGING_SOCK_SVC_ENABLE
    wlan_free_fwr_mem_dump_buffer();
 
-#ifdef WLAN_LOGGING_SOCK_SVC_ENABLE
    if (pHddCtx->cfg_ini->wlanLoggingEnable)
    {
        wlan_logging_sock_deactivate_svc();
@@ -13625,6 +13625,7 @@ void hdd_init_frame_logging_done(void *fwlogInitCbContext, tAniLoggingInitRsp *p
       return;
    }
 
+#ifdef WLAN_LOGGING_SOCK_SVC_ENABLE
    /*Check feature supported by FW*/
    if(TRUE == sme_IsFeatureSupportedByFW(MEMORY_DUMP_SUPPORTED))
    {
@@ -13635,6 +13636,7 @@ void hdd_init_frame_logging_done(void *fwlogInitCbContext, tAniLoggingInitRsp *p
    {
       wlan_store_fwr_mem_dump_size(0);
    }
+#endif
 
 
 }
@@ -14653,10 +14655,9 @@ int hdd_wlan_startup(struct device *dev )
        hddLog(VOS_TRACE_LEVEL_INFO, FL("Logging disabled in ini"));
    }
 
-#endif
-
    if (vos_is_multicast_logging())
        wlan_logging_set_log_level();
+#endif
 
    hdd_register_mcast_bcast_filter(pHddCtx);
 
@@ -14785,8 +14786,8 @@ int hdd_wlan_startup(struct device *dev )
    hdd_init_sw_pta(pHddCtx);
    goto success;
 
-err_open_cesium_nl_sock:
 #ifdef WLAN_LOGGING_SOCK_SVC_ENABLE
+err_open_cesium_nl_sock:
    hdd_close_cesium_nl_sock();
 #endif
 
@@ -16162,6 +16163,7 @@ VOS_STATUS hdd_issta_p2p_clientconnected(hdd_context_t *pHddCtx)
 }
 
 
+#ifdef WLAN_LOGGING_SOCK_SVC_ENABLE
 /*
  * API to find if the firmware will send logs using DXE channel
  */
@@ -16189,6 +16191,7 @@ v_U8_t hdd_is_fw_ev_logging_enabled(void)
     return (pHddCtx && pHddCtx->cfg_ini->wlanLoggingEnable &&
             pHddCtx->cfg_ini->enableFWLogging);
 }
+#endif
 
 /*
  * API to find if there is any session connected
