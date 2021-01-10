33177fa1550c32e9059ef2501251f185cc50dfd7
darkhz <kmachanwenw@gmail.com>
2021-01-10 18:43:27 +0530

    revvz_exec.sh: Set uclamp_latency_sensitive to 0 for all cgroups
    Uclamp's uclamp_latency_sensitive is similar to SchedTune's prefer_idle
    tunable.Enabling this, especially for top-app, foreground etc. will
    cause high battery drain.So we disable it.

--- revvz_exec.sh
+++ revvz_exec.sh
@@ -32,12 +32,12 @@ sysctl -w kernel.sched_util_clamp_min=128
 #top-app
 echo max > /dev/cpuset/top-app/uclamp.max
 echo 85 > /dev/cpuset/top-app/uclamp.min
-echo 1   > /dev/cpuset/top-app/uclamp.latency_sensitive 
+echo 0   > /dev/cpuset/top-app/uclamp.latency_sensitive 
 
 #foreground
 echo 50 > /dev/cpuset/foreground/uclamp.max
 echo 20 > /dev/cpuset/foreground/uclamp.min
-echo 1  > /dev/cpuset/foreground/uclamp.latency_sensitive
+echo 0  > /dev/cpuset/foreground/uclamp.latency_sensitive
 
 #background
 echo max > /dev/cpuset/background/uclamp.max
