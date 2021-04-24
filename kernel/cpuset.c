7e2449226bb55c7b6f17279b3b2d5e75c4464cf2
darkhz <kmachanwenw@gmail.com>
2021-04-24 11:00:11 +0530

    kernel: uclamp_assist: Modify global clamps
    This commit adds support to modify global uclamps on boot.

--- kernel/cpuset.c
+++ kernel/cpuset.c
@@ -61,6 +61,8 @@
 #include <linux/cgroup.h>
 #include <linux/wait.h>
 
+#include <linux/sched/sysctl.h>
+
 DEFINE_STATIC_KEY_FALSE(cpusets_pre_enable_key);
 DEFINE_STATIC_KEY_FALSE(cpusets_enabled_key);
 
@@ -2055,6 +2057,10 @@ static void uclamp_set(struct kernfs_open_file *of,
 			break;
 		}
 	}
+
+	/* Set systemwide uclamps */
+	sysctl_sched_uclamp_util_min = 128;
+	sysctl_sched_uclamp_util_min_rt_default = 500;
 }
 #endif
 
