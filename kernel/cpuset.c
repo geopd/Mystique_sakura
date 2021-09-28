1be4001adefef57456b51792204a05efc1ee1ae8
GeoPD <geoemmanuelpd2001@gmail.com>
2021-09-28 06:59:07 +0000

    cpuset: uclamp_assist: Tune uclamp
    Ref: https://github.com/darkhz/revvz_sakura/blob/beta-4.9-Q/revvz_exec.sh
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>

--- kernel/cpuset.c
+++ kernel/cpuset.c
@@ -2035,11 +2035,11 @@ static void uclamp_set(struct kernfs_open_file *of,
 	const char *cs_name = cs->css.cgroup->kn->name;
 
 	static struct ucl_param tgts[] = {
-		{"top-app",    	     	"10", "100", 1, 1},
-		{"foreground", 	     	"0",  "50",  0, 0},
+		{"top-app",    	     	"20", "100", 0, 1},
+		{"foreground", 	     	"20", "50",  0, 0},
 		{"background", 	     	"20", "100", 0, 0},
-		{"system-background", 	"0",  "40",  0, 0},
-		{"camera-daemon",      	"50", "100", 1, 1},
+		{"system-background", 	"10", "40",  0, 0},
+		{"camera-daemon",      	"0",  "20",  1, 1},
 	};
 
 	for (i = 0; i < ARRAY_SIZE(tgts); i++) {
