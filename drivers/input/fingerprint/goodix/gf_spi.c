7b8ba9518f2719f7639dfb4cef9cd8b72fd89a93
Sultanxda <sultanxda@gmail.com>
2021-09-28 10:21:55 +0530

    drivers: fingerprint: goodix: Set fingerprintd priority to max when screen is off
    Give fingerprintd the highest priority (MIN_NICE) when the screen is off to
    speed up fingerprint processing, and then reset its priority back to normal
    when the screen is on.
    
    There are now 2 fingerprint related processes:
     - android.hardware.biometrics.fingerprint@2.1-service -> comm:fingerprint@2.1
     - com.qualcomm.qti.biometrics.fingerprint.service -> comm:erprint.service
    
    For the match to work properly, use strstr() with "erprint".
    
    Change-Id: Ice4a384f99ae4201b2e5e942b0c8cc16c1190f52
    Co-authored-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>

--- drivers/input/fingerprint/goodix/gf_spi.c
+++ drivers/input/fingerprint/goodix/gf_spi.c
@@ -646,6 +646,18 @@ static const struct file_operations gf_fops = {
 #endif
 };
 
+static void set_fingerprintd_nice(int nice)
+{
+	struct task_struct *p;
+
+	read_lock(&tasklist_lock);
+	for_each_process(p) {
+		if (strstr(p->comm, "erprint"))
+			set_user_nice(p, nice);
+	}
+	read_unlock(&tasklist_lock);
+}
+
 static int goodix_fb_state_chg_callback(struct notifier_block *nb,
 		unsigned long val, void *data)
 {
@@ -666,6 +678,7 @@ static int goodix_fb_state_chg_callback(struct notifier_block *nb,
 		blank = *(int *)(evdata->data);
 		switch (blank) {
 		case FB_BLANK_POWERDOWN:
+			set_fingerprintd_nice(MIN_NICE);
 			if (gf_dev->device_available == 1) {
 				gf_dev->fb_black = 1;
 				gf_dev->wait_finger_down = true;  /* add  for D1S-634 by zhongshengbin */
@@ -679,6 +692,7 @@ static int goodix_fb_state_chg_callback(struct notifier_block *nb,
 			}
 			break;
 		case FB_BLANK_UNBLANK:
+			set_fingerprintd_nice(0);
 			if (gf_dev->device_available == 1) {
 				gf_dev->fb_black = 0;
 #if defined(GF_NETLINK_ENABLE)
